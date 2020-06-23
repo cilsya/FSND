fd64c4c4f4caf87dfb050c38c5bb0367329f2b22
cilsya <cilsya@yahoo.com>
2020-06-22 22:33:26 -0400

    update the environment variable for it to work with Heroku

--- models.py
+++ models.py
@@ -6,15 +6,15 @@ import json
 # https://knowledge.udacity.com/questions/88219
 # In windows console, set the environment
 # set DATABASE_URL='postgres://...'
-#database_path = os.environ['DATABASE_URL']
+database_path = os.environ['DATABASE_URL']
 #database_path = "postgresql://postgres:abcd1234@localhost:5432/herokusample"
 
 # AN ALTERNATIVE
 # https://knowledge.udacity.com/questions/217605
 #default_path=postgres://postgres:postgres@localhost:5432/default_database_path
 #database_path=os.getenv('DATABASE_URL', default_path)
-default_path='postgresql://postgres:abcd1234@localhost:5432/herokusample'
-database_path=os.getenv('DATABASE_URL', default_path)
+# default_path='postgresql://postgres:abcd1234@localhost:5432/herokusample'
+# database_path=os.getenv('DATABASE_URL', default_path)
 
 
 db = SQLAlchemy()
